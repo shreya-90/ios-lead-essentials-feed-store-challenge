6266e730f4ee078112e4cba4476116a98b0c6437
Shreya Pallan <shreya.90@gm>
2021-05-28 17:28:02 +0530

    Check for memory leaks added in makeSUT

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -96,8 +96,15 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	private func makeSUT() -> FeedStore {
         let storeURL = URL(fileURLWithPath: "/dev/null")
 		let sut = try! CoreDataFeedStore(storeURL: storeURL)
+        checkForMemoryLeaks(sut)
         return sut
 	}
+    
+    private func checkForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance,"Instance should have been deallocated. Potential memory leak.",file: file, line: line)
+        }
+    }
 	
 }
 
