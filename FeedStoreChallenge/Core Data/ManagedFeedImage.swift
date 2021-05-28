989a063231342791ac0b89a7e3ca34ffe0430023
Shreya Pallan <shreya.90@gm>
2021-05-28 15:45:09 +0530

    Creating Core Data model classes manually

---
+++ FeedStoreChallenge/Core Data/ManagedFeedImage.swift
@@ -0,0 +1,9 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Shreya Pallan on 28/05/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
