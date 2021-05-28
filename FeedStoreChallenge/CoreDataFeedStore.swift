939a6903f752385475d2c5e6d891392c7cf91c2f
Shreya Pallan <shreya.90@gm>
2021-05-28 18:11:13 +0530

    Dispatch CoreDataFeedStore operations in a serail background queue to avoid blocking clients

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -27,9 +27,12 @@ public final class CoreDataFeedStore: FeedStore {
 		)
 		context = container.newBackgroundContext()
 	}
+    
+    private let queue = DispatchQueue(label: "\(CoreDataFeedStore.self)Queue", qos: .userInitiated)
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-          perform { context in
+        queue.async { [unowned self] in
+            self.perform { context in
                     do {
                             if let cache = try ManagedCache.find(in: context) {
                                 completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
@@ -40,31 +43,36 @@ public final class CoreDataFeedStore: FeedStore {
                             completion(.failure(error))
                         }
                 }
+        }
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        perform { context in
-                do {
-                        let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                        managedCache.timestamp = timestamp
-                        managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                        try context.save()
-                        completion(nil)
-                    } catch {
-                        completion(error)
-                    }
+        queue.async { [unowned self] in
+            self.perform { context in
+                    do {
+                            let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                            managedCache.timestamp = timestamp
+                            managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+                            try context.save()
+                            completion(nil)
+                        } catch {
+                            completion(error)
+                        }
+            }
         }
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
-            do {
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
-                completion(nil)
-            } catch {
-                completion(error)
+        queue.async { [unowned self] in
+            self.perform { context in
+                do {
+                    try ManagedCache.find(in: context).map(context.delete).map(context.save)
+                    completion(nil)
+                } catch {
+                    completion(error)
+                }
+                
             }
-            
         }
 	}
     
