0a8eca58cb59d565a6423e09ef36b51eeff97447
Shreya Pallan <shreya.90@gm>
2021-05-29 17:51:01 +0530

    Adding rollback in insert and delete error scenarios to restore previous state and avoid side-effects

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -56,6 +56,7 @@ public final class CoreDataFeedStore: FeedStore {
                             try context.save()
                             completion(nil)
                         } catch {
+                            context.rollback()
                             completion(error)
                         }
             }
@@ -69,6 +70,7 @@ public final class CoreDataFeedStore: FeedStore {
                     try ManagedCache.find(in: context).map(context.delete).map(context.save)
                     completion(nil)
                 } catch {
+                    context.rollback()
                     completion(error)
                 }
                 
